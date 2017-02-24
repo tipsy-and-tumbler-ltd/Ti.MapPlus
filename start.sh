ba59ec56d3842f60d93174d64a58a90add4a6795
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-02-24 21:10:52 +0100

    new project structure

--- start.sh
+++ start.sh
@@ -5,5 +5,5 @@ VERSION=2.4.0
 
 #cd android; ant clean; rm -rf /dist/*;rm -rf build/*; ant ;unzip -uo  dist/$APPID-android-$VERSION.zip  -d  ~/Documents/APPC_WORKSPACE/FlicTest/; cd  ..
 
-cd android; ant clean; rm -rf /dist/*;rm -rf build/*; ant ;unzip -uo  dist/$APPID-android-$VERSION.zip  -d  ~/Documents/APPC_WORKSPACE/Hafenradler/; cd  ..
+cd android/ti.map; ant clean; rm -rf /dist/*;rm -rf build/*; ant ;unzip -uo  dist/$APPID-android-$VERSION.zip  -d  ~/Documents/APPC_WORKSPACE/Hafenradler/; cd  ..
 
