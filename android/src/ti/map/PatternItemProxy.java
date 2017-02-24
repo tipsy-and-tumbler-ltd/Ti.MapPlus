8193d4784d48878dc05b7fbee1e35be98afa83dc
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2017-02-24 11:33:30 +0100

             

--- android/src/ti/map/PatternItemProxy.java
+++ android/src/ti/map/PatternItemProxy.java
@@ -7,44 +7,20 @@
 package ti.map;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollProxy;
 import org.appcelerator.kroll.annotations.Kroll;
-import org.appcelerator.kroll.common.AsyncResult;
-import org.appcelerator.kroll.common.TiMessenger;
-import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.util.TiConvert;
 
-import android.os.Message;
-
+import com.google.android.gms.maps.model.Dash;
 import com.google.android.gms.maps.model.Dot;
 import com.google.android.gms.maps.model.Gap;
-import com.google.android.gms.maps.model.Dash;
-import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PatternItem;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.maps.android.PolyUtil;
 
 @Kroll.proxy(creatableInModule = MapModule.class)
 public class PatternItemProxy extends KrollProxy {
-	private static final int COLOR_BLACK_ARGB = 0xff000000;
-	private static final int COLOR_WHITE_ARGB = 0xffffffff;
-	private static final int COLOR_GREEN_ARGB = 0xff388E3C;
-	private static final int COLOR_PURPLE_ARGB = 0xff81C784;
-	private static final int COLOR_ORANGE_ARGB = 0xffF57F17;
-	private static final int COLOR_BLUE_ARGB = 0xffF9A825;
-
-	private static final int POLYGON_STROKE_WIDTH_PX = 8;
-	private static final int PATTERN_DASH_LENGTH_PX = 20;
-	private static final int PATTERN_GAP_LENGTH_PX = 20;
-	private static final PatternItem DASH = new Dash(PATTERN_DASH_LENGTH_PX);
-	private static final PatternItem DOT = new Dot();
-	private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
 	private float strokeWidth = 5f;
 	private float dashLength = 20f;
 	private float gapLength = 20f;
@@ -54,6 +30,10 @@ public class PatternItemProxy extends KrollProxy {
 
 	public PatternItemProxy() {
 		super();
+	}
+
+	public List<PatternItem> getPattern() {
+		return patternItems;
 	}
 
 	public void handleCreateDict(KrollDict opts) {
